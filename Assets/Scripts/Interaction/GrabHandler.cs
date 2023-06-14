b18a4953c578d88d8b4aa1d7d898dc8e15591307
hunterdyar <bloop@hdyar.com>
2023-06-14 13:18:24 -0400

    hanfle joint breaking

--- Assets/Scripts/Interaction/GrabHandler.cs
+++ Assets/Scripts/Interaction/GrabHandler.cs
@@ -35,16 +35,28 @@ namespace Interaction
 			_grabJoint.connectedAnchor = transform.localPosition;
 			_grabJoint.enableCollision = true;
 			_grabJoint.spring = 500;
-
 			_grabJoint.damper = 20;
 			_grabJoint.connectedBody = playerBody;//this should snap to our hands now.
+			
+			grabbable.Grabbed(this);
 			_holdingGrabbable = grabbable;
 		}
 
 		public void Throw(Vector3 throwForce)
 		{
+			_holdingGrabbable.Released();
 			Destroy(_grabJoint);
 			_holdingGrabbable.Rigidbody.AddForce(throwForce,ForceMode.Impulse);
+			_holdingGrabbable = null;
+		}
+
+		//so the grabbable gets the event for joints breaking, but we want to handle releasing
+		//so it calls this, then we tell it "yeah okay you release". which, yes, is dumb.
+		public void ForceRelease()
+		{
+			Destroy(_grabJoint);
+			_holdingGrabbable.Released();
+			_holdingGrabbable = null;
 		}
 	}
 }
--- Assets/Scripts/Interaction/Grabbable.cs
+++ Assets/Scripts/Interaction/Grabbable.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Interaction;
 using UnityEngine;
 
 public class Grabbable : Interactable
@@ -7,8 +9,30 @@ public class Grabbable : Interactable
 	public Rigidbody Rigidbody => _rigidbody;
 	private Rigidbody _rigidbody;
 
+	private GrabHandler _grabHandler;
+	
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	public void Grabbed(GrabHandler grabHandler)
+	{
+		_grabHandler = grabHandler;
+		SetInteracting(true);
+	}
+
+	public void Released()
+	{
+		_grabHandler = null;
+		SetInteracting(false);
+	}
+
+	private void OnJointBreak(float breakForce)
+	{
+		if (_grabHandler != null)
+		{
+			_grabHandler.ForceRelease();
+		}
 	}
 }
